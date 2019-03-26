f1b608373815a57478c3a0e8f3237edaa426fe29
Heath JH Moore <heathjhmoore@gmail.com>
2019-03-25 20:57:37 -0500

    added array of cards

--- project.js
+++ project.js
@@ -1,13 +1,30 @@
-console.log("whats up");
+const ingredients = [];
 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML += textToPrint;
+    selectedDiv.innerHTML = textToPrint;
 };
+
+const domStringBuilder = (arrayToPrint) => {
+    let domString = ``;
+    arrayToPrint.forEach((ingredient) => {
+        domString += `<div class="card col-8">`
+        domString +=    `<div class="card-body">`
+        domString +=      `<h5 class="card-title">Card title</h5>`
+        domString +=      `<a href="#" class="btn btn-primary">Delete</a>`
+        domString +=    `</div>`
+        domString += `</div>`
+    })
+    printToDom('ingredient-container', domString);
+}
 
 const addIngredient = () => {
     const inputText = document.getElementById("ingredientInput").value;
-    printToDom('ingredient-container', inputText);
+    const newIngredient = {
+        item: inputText,
+    }
+    ingredients.push(newIngredient);
+    domStringBuilder(ingredients);
     document.getElementById('ingredientInput').value = ``;
 }
 
